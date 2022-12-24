c9884d9976c9d1134a98bdbfca212c3ce0225f58
Nir Tamir <nirtamir2@gmail.com>
2022-12-24 19:10:32 +0200

    simplify functions

--- src/rules/i18n-prefix.ts
+++ src/rules/i18n-prefix.ts
@@ -2,7 +2,7 @@ import type { Rule, Scope } from "eslint";
 
 function isValidComponentName(name: string): boolean {
   // A valid component name must start with an uppercase letter
-  const firstChar = name[0];
+  const [firstChar] = name;
   if (firstChar == null || firstChar !== firstChar.toUpperCase()) {
     return false;
   }
@@ -16,37 +16,35 @@ function getErrorMessage({ expected, got }: { expected: string; got: string }) {
   return `i18n translation key does not start with component name. Expected "${expected}" but got "${got}".`;
 }
 
-function getNearestComponentNameInHierarchy(
-  currentScope: Scope.Scope
-): string | null {
-  let result: Scope.Scope | null = currentScope;
-  while (
-    (result != null && result.type !== "function") ||
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    !isValidComponentName(
-      /*try function declaration */
+function getParentComponentName(initialScope: Scope.Scope): string | null {
+  for (
+    let resultScope: Scope.Scope | null = initialScope;
+    resultScope != null;
+    resultScope = resultScope.upper
+  ) {
+    if (resultScope.type !== "function") {
+      continue;
+    }
+
+    const { block } = resultScope;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const functionName: string | null =
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument
-      result.block.id?.name ??
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        /*result.block.type === "ArrowFunctionExpression"*/ result?.block.parent
-          .id?.name ??
-        /* Maybe it's inside an anonymous function */ ""
-    )
-  ) {
-    result = currentScope.upper;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      block.id?.name ??
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      /*result.block.type === "ArrowFunctionExpression"*/ block.parent.id?.name;
+
+    if (functionName != null && isValidComponentName(functionName)) {
+      return functionName;
+    }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const componentName: string | null =
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    result.block.id?.name ?? result?.block.parent.id.name;
-  return componentName;
+  return null;
 }
 
 export const i18nPrefix: Rule.RuleModule = {
@@ -95,17 +93,21 @@ export const i18nPrefix: Rule.RuleModule = {
           return;
         }
 
-        const componentName = getNearestComponentNameInHierarchy(
-          context.getScope()
-        );
+        const componentName = getParentComponentName(context.getScope());
 
         if (componentName == null) {
+          context.report({
+            node,
+            message: "t function not found inside a component",
+          });
           return;
         }
 
+        // t("string.literal")
         if (firstArg.type === "Literal" && typeof firstArg.value === "string") {
           const { range, value } = firstArg;
-          const [firstI18nKeyPart, ...restI18nKey] = value.split(delimiter);
+          const [firstI18nKeyPart, ...restI18nKeyParts] =
+            value.split(delimiter);
           if (firstI18nKeyPart == null || range == null) {
             return;
           }
@@ -119,36 +121,37 @@ export const i18nPrefix: Rule.RuleModule = {
               }),
               fix(fixer) {
                 // fix function: replace the first part of the string literal with the function name
-                const newValue = `${componentName}.${restI18nKey.join(
+                const newValue = [componentName, ...restI18nKeyParts].join(
                   delimiter
-                )}`;
+                );
                 const [start, end] = range;
                 return fixer.replaceTextRange([start + 1, end - 1], newValue);
               },
             });
           }
+          // t(`template.string.with.${parameter}`)
         } else if (
           firstArg.type === "TemplateLiteral" &&
           typeof firstArg.quasis[0]?.value.raw === "string"
         ) {
-          const parts = firstArg.quasis[0].value.raw.split(delimiter);
+          const [firstI18nKeyPart, ...restI18nKeyParts] =
+            firstArg.quasis[0].value.raw.split(delimiter);
           const { range } = firstArg.quasis[0];
-          const firstPart = parts[0];
-          if (firstPart == null || range == null) {
+          if (firstI18nKeyPart == null || range == null) {
             return;
           }
 
-          if (componentName !== firstPart) {
+          if (componentName !== firstI18nKeyPart) {
             context.report({
               node,
               message: getErrorMessage({
                 expected: componentName,
-                got: firstPart,
+                got: firstI18nKeyPart,
               }),
               fix(fixer) {
-                const newValue = `${componentName}.${parts
-                  .slice(1)
-                  .join(delimiter)}`;
+                const newValue = [componentName, ...restI18nKeyParts].join(
+                  delimiter
+                );
                 const [start, end] = range;
                 return fixer.replaceTextRange([start + 1, end - 2], newValue);
               },
